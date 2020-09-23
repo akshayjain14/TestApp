d73011d2ad8b72b3c5c2b98bb408cea935ea83ba
Akshay Jain <jain05akshay@gmail.com>
2020-09-23 17:21:15 +0530

    model data

---
+++ SoftvisionTestApp/Model/ImageModel.swift
@@ -0,0 +1,26 @@
+//
+//  ImageModel.swift
+//  SoftvisionTestApp
+//
+//  Created by Akshay Jain on 23/09/20.
+//  Copyright © 2020 akshay. All rights reserved.
+//
+
+import Foundation
+struct ImageModel : Codable {
+    let title : String?
+    let rows : [Rows]?
+
+    enum CodingKeys: String, CodingKey {
+
+        case title = "title"
+        case rows = "rows"
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        title = try values.decodeIfPresent(String.self, forKey: .title)
+        rows = try values.decodeIfPresent([Rows].self, forKey: .rows)
+    }
+
+}
