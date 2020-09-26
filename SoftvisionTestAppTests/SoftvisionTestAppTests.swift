26a8e6184c6a313c3800b6a643e319995ba55f61
Akshay Jain <jain05akshay@gmail.com>
2020-09-26 10:22:44 +0530

    code correction

--- SoftvisionTestAppTests/SoftvisionTestAppTests.swift
+++ SoftvisionTestAppTests/SoftvisionTestAppTests.swift
@@ -12,17 +12,16 @@ import XCTest
 
 class SoftvisionTestAppTests: XCTestCase {
 
-    var sut: URLSession!
+   
     var viewModel: ViewModel?
     
     override func setUpWithError() throws {
         super.setUp()
-        sut = URLSession(configuration: .default)
         viewModel = ViewModel()
     }
 
     override func tearDownWithError() throws {
-        sut = nil
+        
         super.tearDown()
     }
 
@@ -30,37 +29,14 @@ class SoftvisionTestAppTests: XCTestCase {
         let promise = expectation(description: "fetchedSuccess")
         viewModel =  ViewModel()
         viewModel?.bindViewModelToController = {
-            guard let imageData = viewModel?.imageModel.rows else {
+            guard (self.viewModel?.imageModel.rows) != nil else {
                 XCTFail("Failed")
                 return
            }
             promise.fulfill()
         }
         wait(for: [promise], timeout: 10)
-        
-      
-//      let url =
-//        URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json")
-//
-//      let promise = expectation(description: "Status code: 200")
-//
-//      let dataTask = sut.dataTask(with: url!) { _, response, error in
-//
-//        if let error = error {
-//          XCTFail("Error: \(error.localizedDescription)")
-//          return
-//        } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-//          if statusCode == 200 {
-//
-//            promise.fulfill()
-//          } else {
-//            XCTFail("Status code: \(statusCode)")
-//          }
-//        }
-//      }
-//      dataTask.resume()
-//
-//      wait(for: [promise], timeout: 10)
+
    }
 
 }
